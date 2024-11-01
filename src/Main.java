c92b63f9bdc94b6c1f820a3a1cba8aec560e03f5
luisona5 <150484099+luisona5@users.noreply.github.com>
2024-11-01 10:04:20 -0500

    cuadrado

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,17 @@
 import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Cuadrado cuadrado1 = new Cuadrado(2.0,2.0,2.0,2.0);
+        cuadrado1.CuadradoPerimetro();
+        cuadrado1.CuadradoArea();
+        cuadrado1.setLado1(3.0);
+        cuadrado1.setLado2(3.0);
+        cuadrado1.setLado3(3.0);
+        cuadrado1.setLado4(3.0);
+        cuadrado1.CuadradoPerimetro();
+        cuadrado1.CuadradoArea();
+
+    }
+}
