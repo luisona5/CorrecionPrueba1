ff2c8bcb15065d22ce54c1c074ac07b0c2f2f0f1
luisona5 <150484099+luisona5@users.noreply.github.com>
2024-11-01 21:46:15 -0500

    triangulo

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,7 @@ public class Main {
         Cuadrado cuadrado1 = new Cuadrado(2.0);
         cuadrado1.CuadradoPerimetro();
         cuadrado1.CuadradoArea();
-        cuadrado1.setLado1(8.0);
-        cuadrado1.CuadradoPerimetro();
-        cuadrado1.CuadradoArea();
+
 
         Rectangulo rectangulo1=new Rectangulo(2,3);
         rectangulo1.Perimetro();
@@ -18,9 +16,34 @@ public class Main {
         circulo1.Perimetro();
         circulo1.Area();
 
+
         Triangulo triangulo1=new Triangulo(2,3,4);
         triangulo1.Perimetro();
         triangulo1.Area();
+
+        System.out.println("----------- CON VALORES SETEADOS----------");
+
+        // -----------VALORES SETEADOS-------------
+        cuadrado1.setLado1(8.0);
+        cuadrado1.CuadradoPerimetro();
+        cuadrado1.CuadradoArea();
+        //--------------------------------------
+        rectangulo1.setBase(8);
+        rectangulo1.setAltura(4);
+        rectangulo1.Perimetro();
+        rectangulo1.Area();
+        //-------------------------------------------
+        circulo1.setRadio(5.9);
+        circulo1.Perimetro();
+        circulo1.Area();
+        //-------------------------------------------
+        triangulo1.setAltura(10);
+        triangulo1.setBase(3);
+        triangulo1.Perimetro();
+        triangulo1.Area();
+
+
+        ;
 
     }
 }
